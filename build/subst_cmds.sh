231c44f5e68947103a27e560b0712abb1c13efc9
C6H5-NO2 <19368807+C6H5-NO2@users.noreply.github.com>
2023-07-08 19:26:35 +0800

    add auto update script
    todo setup workflow

---
+++ build/subst_cmds.sh
@@ -0,0 +1,1 @@
+WEBGAL_CMD=$1 envsubst '$WEBGAL_CMD' < build/webgal.tmLanguage.json.tpl > syntaxes/webgal.tmLanguage.json
---
+++ build/update_cmds.py
@@ -0,0 +1,21 @@
+import re
+from http import HTTPStatus
+from http.client import HTTPResponse
+from urllib.request import urlopen
+
+
+def main():
+    url = 'https://raw.githubusercontent.com/MakinoharaShoko/WebGAL/main/packages/parser/src/config/scriptConfig.ts'
+    with urlopen(url) as response:  # type: HTTPResponse
+        if response.status != HTTPStatus.OK:
+            raise RuntimeError(f'HTTP GET failed with {response.status}')
+        body = response.read().decode('utf-8')
+    commands = [m.group(1) for m in re.finditer(r"scriptString:\s*'([^']+)'", body)]
+    commands.sort()
+    print(f'Found {len(commands)} commands: {commands}')
+    pattern = '|'.join(commands)
+    print(pattern)
+
+
+if __name__ == '__main__':
+    main()
